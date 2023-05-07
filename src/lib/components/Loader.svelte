955c6d07131a732e06532b88f1e946c03148e75a
dagregi <120724828+dagregi@users.noreply.github.com>
2023-05-07 17:02:34 +0300

    feat: Better search with less cluter

---
+++ src/lib/components/Loader.svelte
@@ -0,0 +1,9 @@
+<ul class="list">
+  <li>
+    <div class="w-14 h-14 placeholder animate-pulse" />
+    <span class="flex-auto space-y-2">
+      <div class="placeholder animate-pulse" />
+      <div class="placeholder w-1/2 animate-pulse" />
+    </span>
+  </li>
+</ul>
--- src/routes/(search)/search/+layout.svelte
+++ src/routes/(search)/search/+layout.svelte
@@ -1,1 +1,16 @@
-<slot />
+<script>
+  import { browser } from "$app/environment";
+  import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
+
+  const queryClieant = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser,
+      },
+    },
+  });
+</script>
+
+<QueryClientProvider client={queryClieant}>
+  <slot />
+</QueryClientProvider>
